aee095b4dd7a98fb3ce8665a088bce01fb6fd943
Sajan Ngk <87972993+SajanNgk@users.noreply.github.com>
2024-08-08 11:05:56 +0545

    base_changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
-## 0.0.1
+## 0.1.0
 
-* TODO: Describe initial release.
+* Initial release of the graphql_repository package.
+* Implemented GraphQLBaseRepository with query, mutate, and subscribe methods.
+* Added custom API exceptions handling.
+* Included logging functionality.
--- test/raphql_repository_test.dart
+++ test/raphql_repository_test.dart
@@ -1,43 +1,74 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:graph_base_repo/graphql_repository.dart';
-
+import 'package:graphql_repository/graphql_repository.dart';
 import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
-
-// This will generate a MockGraphQLClient
-@GenerateMocks([GraphQLClient])
-
+// This will generate a MockGraphQLClient and MockLink
+@GenerateMocks([GraphQLClient, Link])
 import 'raphql_repository_test.mocks.dart';
 
 class TestRepository extends GraphQLBaseRepository {
   TestRepository({required GraphQLClient client})
-      : super(endpoint: 'https://graphql.anilist.co', link: client.link);
+      : super(
+            endpoint: 'https://restronaut.hyperce.io/shop-api/',
+            link: client.link);
 }
 
 void main() {
   late MockGraphQLClient mockClient;
+  late MockLink mockLink;
   late TestRepository repository;
 
   setUp(() {
     mockClient = MockGraphQLClient();
+    mockLink = MockLink();
+    when(mockClient.link).thenReturn(mockLink);
+
+    // Stubbing the concat method to return a mockLink when called
+    when(mockLink.concat(any)).thenReturn(mockLink);
+
+    // Stubbing the request method to return a specific response
+    when(mockLink.request(any, any)).thenAnswer((_) {
+      const response = Response(data: {
+        'channels': {
+          'items': [
+            {'id': '2', 'code': 'Restro One', 'token': 'restro-one'},
+            {'id': '3', 'code': 'Restro two', 'token': 'restro-two'}
+          ]
+        }
+      }, response: {
+        'headers': <String, String>{'content-type': 'application/json'}
+      });
+      return Stream.value(response);
+    });
+
     repository = TestRepository(client: mockClient);
   });
 
   group('GraphQLBaseRepository', () {
     test('query should return data on success', () async {
       const testQuery = '''
-        query TestQuery {
-          test {
-            id
-            name
+        query Channels {
+          channels {
+            items {
+              id
+              code
+              token
+            }
           }
         }
       ''';
 
       final expectedData = {
-        'test': {'id': '1', 'name': 'Test Name'}
+        'data': {
+          'channels': {
+            'items': [
+              {'id': '2', 'code': 'Restro One', 'token': 'restro-one'},
+              {'id': '3', 'code': 'Restro two', 'token': 'restro-two'}
+            ]
+          }
+        }
       };
 
       when(mockClient.query(any)).thenAnswer((_) async => QueryResult(
@@ -64,7 +95,9 @@ void main() {
       when(mockClient.query(any)).thenAnswer((_) async => QueryResult(
             source: QueryResultSource.network,
             exception: OperationException(
-              graphqlErrors: [GraphQLError(message: 'Test GraphQL Error')],
+              graphqlErrors: [
+                const GraphQLError(message: 'Test GraphQL Error')
+              ],
             ),
             options: QueryOptions(document: gql(testQuery)),
           ));
